7a1cfd3b57194003fd40df137cd43ad67a058476
Brigthain Kargong <kbrigthain@gmail.com>
2025-06-29 22:19:47 -0400

    Test

--- terraform/formations/Create-Tenant-resources/variables.tf
+++ terraform/formations/Create-Tenant-resources/variables.tf
@@ -13,40 +13,38 @@ variable "common" {
 variable "ec2s" {
   description = "EC2 Instance configuration"
   type = list(object({
-    type = object({
-      index         = optional(string)
-      name          = string
-      name_override = optional(string)
-      custom_ami    = optional(string)
-      ami_config = object({
-        os_release_date  = optional(string)
-        os_base_packages = optional(string)
-      })
-      associate_public_ip_address = optional(bool, false)
-      instance_type               = string
-      iam_instance_profile        = string
-      key_name                    = string
-      custom_tags                 = optional(map(string))
-      ebs_root_volume = optional(object({
-        volume_size           = number
-        volume_type           = optional(string, "gp3")
-        delete_on_termination = optional(bool, true)
-        encrypted             = optional(bool, false)
-        kms_key_id            = optional(string, null)
-      }))
-      ebs_device_volume = optional(object({
-        name                  = string
-        volume_size           = number
-        volume_type           = optional(string, "gp3")
-        delete_on_termination = optional(bool, true)
-        encrypted             = optional(bool, false)
-        kms_key_id            = optional(string, null)
-      }))
-      subnet_id          = string
-      Schedule_name      = optional(string)
-      backup_plan_name   = optional(string)
-      security_group_ids = list(string)
+    index         = optional(string)
+    name          = string
+    name_override = optional(string)
+    custom_ami    = optional(string)
+    ami_config = object({
+      os_release_date  = optional(string)
+      os_base_packages = optional(string)
     })
+    associate_public_ip_address = optional(bool, false)
+    instance_type               = string
+    iam_instance_profile        = string
+    key_name                    = string
+    custom_tags                 = optional(map(string))
+    ebs_root_volume = optional(object({
+      volume_size           = number
+      volume_type           = optional(string, "gp3")
+      delete_on_termination = optional(bool, true)
+      encrypted             = optional(bool, false)
+      kms_key_id            = optional(string, null)
+    }))
+    ebs_device_volume = optional(object({
+      name                  = string
+      volume_size           = number
+      volume_type           = optional(string, "gp3")
+      delete_on_termination = optional(bool, true)
+      encrypted             = optional(bool, false)
+      kms_key_id            = optional(string, null)
+    }))
+    subnet_id          = string
+    Schedule_name      = optional(string)
+    backup_plan_name   = optional(string)
+    security_group_ids = list(string)
     hosted_zones = optional(object({
       name           = string
       zone_id        = string
